e27e5bd60b99bb4b5a7085b98f6802281c6823ff
Pazbear <mkcho1997@daum.net>
2020-08-03 17:06:15 +0900

    testing

--- frontend/frontend/src/components/views/LandingPage/LandingPageVote.js
+++ frontend/frontend/src/components/views/LandingPage/LandingPageVote.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 import RankBox from './Sections/RankBox'
 import VoteBox from './Sections/VoteBox'
 import './Sections/Vote.css'
 function LandingPageVote(props) {
+
+    useEffect(() => {
+        axios.get('/profile')
+        .then(response => {
+            console.log(response)
+        })
+    }, [])
 
     return (
         <>
