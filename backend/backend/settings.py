d02e0acebc6971aa9ffc47f98fdacfb5003a384b
VIXXPARK <vixx170627@gmail.com>
2020-08-28 17:00:28 +0900

    private to public

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q447m#bi8j30@q(72b2-kxr^ubb241g596&epaazu^6fu95$l8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
