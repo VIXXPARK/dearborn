fb40a20a50ea737308cea9a4e125d8c80eaf1646
VIXXPARK <vixx170627@gmail.com>
2020-10-09 14:51:45 +0900

    qw

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,9 +112,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(REACT_DIR,'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(REACT_DIR,'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
