d13d8dfde3836f6d41f8d7ccf00af87b434bc23b
VIXXPARK <vixx170627@gmail.com>
2020-10-09 12:56:52 +0900

    re_path

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import index
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('usermanagement.urls')),
     path('api/', include('post.urls')),
@@ -29,4 +28,5 @@ urlpatterns = [
     path('api/',include('messanger.urls')),
     path('api/',include('contest.urls')),
     path('api/',include('assess.urls')),
+    re_path(".*",TemplateView.as_view(template_name="index.html")),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
