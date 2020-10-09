d384940fbb1aa10086ac669242caf31c89960adb
VIXXPARK <vixx170627@gmail.com>
2020-10-09 13:49:14 +0900

    kk

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(REACT_DIR,'build')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
