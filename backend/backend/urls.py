cca3bde5d6f7fcc632f716122b06cc9a0de4a15a
VIXXPARK <vixx170627@gmail.com>
2020-10-09 13:48:05 +0900

    ㅠㅠ

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
+from .views import FrontendAppView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +28,5 @@ urlpatterns = [
     path('api/',include('messanger.urls')),
     path('api/',include('contest.urls')),
     path('api/',include('assess.urls')),
-    re_path(".*",TemplateView.as_view(template_name="index.html")),
+    re_path(r'^', FrontendAppView.as_view()),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
