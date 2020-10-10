bba873eaffe147dd7ad34a6496d240054b29e951
VIXXPARK <vixx170627@gmail.com>
2020-10-10 20:07:15 +0900

    dd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(REACT_DIR,'build','static')
 
 STATICFILES_DIRS = (
-    os.path.join(REACT_DIR,'build''static'),
+    os.path.join(REACT_DIR,'build','static'),
     os.path.join(REACT_DIR,'build')
 )
 
