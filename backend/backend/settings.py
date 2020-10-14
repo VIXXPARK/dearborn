f4d783620f5d0e6b0dd7746bb9e2119d1fc170e8
VIXXPARK <vixx170627@gmail.com>
2020-10-14 18:56:13 +0900

    index fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -115,7 +115,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # STATIC_ROOT = os.path.join(REACT_DIR,'build','static')
 
 STATICFILES_DIRS = (
-    os.path.join(REACT_DIR,'build','static'),
+    os.path.join(REACT_DIR,'app','build','static'),
+    # os.path.join(BASE_DIR,'static'),
     # os.path.join(REACT_DIR,'build'),
     # os.path.join(REACT_DIR,'build','static','css'),
 )
