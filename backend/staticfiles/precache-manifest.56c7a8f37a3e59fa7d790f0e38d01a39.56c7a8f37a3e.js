0e4a75c80f5b45c13c04cbcf39969275b59b87bf
VIXXPARK <vixx170627@gmail.com>
2020-10-14 15:38:47 +0900

    staticfiles

---
+++ backend/staticfiles/precache-manifest.56c7a8f37a3e59fa7d790f0e38d01a39.56c7a8f37a3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6fd946e5ab5e0bd8ee379c1f64808e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6e82a5c36d6b8af3a44",
+    "url": "./static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "069e566f6a8804260a6b",
+    "url": "./static/css/main.6061fc23.chunk.css"
+  },
+  {
+    "revision": "e6e82a5c36d6b8af3a44",
+    "url": "./static/js/2.e94f0a5e.chunk.js"
+  },
+  {
+    "revision": "edb3e9b961f63df7902af1e966a3e175",
+    "url": "./static/js/2.e94f0a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "069e566f6a8804260a6b",
+    "url": "./static/js/main.c26acf8d.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  },
+  {
+    "revision": "12ddf182853fc0f311389c130e8e8443",
+    "url": "./static/media/Banner.12ddf182.jpg"
+  },
+  {
+    "revision": "654c91e7fea3966664183c1fedcf1682",
+    "url": "./static/media/Sketch.654c91e7.png"
+  }
+]);
