373963b40948f4bac09bb0dff8d217ea57981560
VIXXPARK <vixx170627@gmail.com>
2020-10-16 16:11:11 +0900

    settings 분리

---
+++ backend/backend/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dearborn',
+        'USER': 'jinminsu200703',
+        'PASSWORD': 'H0@6340@8740$801',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
