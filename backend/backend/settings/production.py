ef3a6eeaca0d1250f495736084d768d76d0c8ce4
HJinS <windowed@naver.com>
2020-10-22 16:21:57 +0900

    send

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -12,9 +12,8 @@ MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
 EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
-EMAIL_HOST_USER = os.environ.get("SENDGRID_USERNAME")
-EMAIL_HOST_PSSSWORD = os.environ.get("SENDGRID_PASSWORD")
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PSSSWORD = os.environ.get("SENDGRID_API_KEY")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
