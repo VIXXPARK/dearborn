baec227c728653dfee66eb20e23b24bbc7574eda
HJinS <windowed@naver.com>
2020-10-22 23:20:19 +0900

    testBuild

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -10,13 +10,23 @@ ALLOWED_HOSTS = ['*']
 
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
-EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_HOST_USER = os.environ.get("SENDGRID_USERNAME")
-EMAIL_HOST_PSSSWORD = os.environ.get("SENDGRID_PASSWORD")
-EMAIL_PORT = os.environ.get("EMAIL_PORT")
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+# EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+# EMAIL_HOST = os.environ.get("EMAIL_HOST")
+# EMAIL_HOST_USER = os.environ.get("SENDGRID_USERNAME")
+# EMAIL_HOST_PSSSWORD = os.environ.get("SENDGRID_PASSWORD")
+# EMAIL_PORT = os.environ.get("EMAIL_PORT")
+# EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+# DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'app186831835@heroku.com'
+EMAIL_HOST_PSSSWORD = 'gicnld779816'
+EMAil_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'dearborn0819@gmail.com'
+
+
 REDIRECT_PAGE = os.environ.get("REDIRECT_PAGE")
 REDIRECT_PAGE_FAILED = os.environ.get("REDIRECT_PAGE_FAILED")
 SECRET_KEY = os.environ.get("SECRET_KEY")
