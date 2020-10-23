6c1e86593ffef017bce00c9b346674ed6aca7223
HJinS <windowed@naver.com>
2020-10-23 20:20:38 +0900

    tmp

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -15,19 +15,17 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_HOST_USER = "apikey"
-EMAIL_HOST_PASSWORD = "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"
-REDIRECT_PAGE = 'http://localhost:3000'
-REDIRECT_PAGE_FAILED = 'http://localhost:3000/checkEmail/failed'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+REDIRECT_PAGE = 'http://dearborn.herokuapp.com'
+REDIRECT_PAGE_FAILED = 'http://dearborn.herokuapp.com/failed'
 DEFAULT_FROM_EMAIL = "dearborn0819@gmail.com"
 
 EMAIL['REDIRECT_PAGE'] = REDIRECT_PAGE
@@ -45,5 +43,9 @@ STATIC_URL = 'https://%s/%s/' %(AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
 
 STATICFILES_STORAGE = 'backend.storage.S3StaticStorage'
 DEFAULT_FILE_STORAGE = 'backend.storage.S3MediaStorage'
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 django_heroku.settings(locals())
