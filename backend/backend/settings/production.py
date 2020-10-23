f6cccb5ff636aed2d1590ea75ed99faf75e36c5b
HJinS <windowed@naver.com>
2020-10-23 19:22:38 +0900

    제발

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -10,50 +10,52 @@ ALLOWED_HOSTS = ['*']
 
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
-EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-# EMAIL_HOST_USER = os.environ.get("SENDGRID_USERNAME")
-# EMAIL_HOST_PSSSWORD = os.environ.get("SENDGRID_PASSWORD")
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PSSSWORD = os.environ.get("SENDGRID_API_KEY")
-# EMAIL_PORT = os.environ.get("EMAIL_PORT")
-# EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
-
-REDIRECT_PAGE = os.environ.get("REDIRECT_PAGE")
-REDIRECT_PAGE_FAILED = os.environ.get("REDIRECT_PAGE_FAILED")
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
-SERVER_EMAIL = os.environ.get("SERVER_EMAIL")
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-
-AWS_REGION = os.environ.get("AWS_S3_REGION_NAME")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_CUSTOM_DOMAIN='%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
-AWS_DEFAULT_ACL = None
-AWS_LOCATION='static'
-STATIC_URL = 'https://%s/%s/' %(AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dearborn',
+        'USER': 'jinminsu200703',
+        'PASSWORD': 'H0@6340@8740$801',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
     }
 }
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("SENDGRID_USERNAME")
+EMAIL_HOST_PSSSWORD = os.environ.get("SENDGRID_PASSWORD")
+REDIRECT_PAGE = os.environ.get("REDIRECT_PAGE")
+REDIRECT_PAGE_FAILED = os.environ.get("REDIRECT_PAGE_FAILED")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 EMAIL['REDIRECT_PAGE'] = REDIRECT_PAGE
 EMAIL['REDIRECT_PAGE_FAILED'] = REDIRECT_PAGE_FAILED
 
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.environ.get("AWS_S3_REGION_NAME")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+
+AWS_S3_CUSTOM_DOMAIN='%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+AWS_DEFAULT_ACL = None
+AWS_LOCATION='static'
+STATIC_URL = 'https://%s/%s/' %(AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
+
 STATICFILES_STORAGE = 'backend.storage.S3StaticStorage'
 DEFAULT_FILE_STORAGE = 'backend.storage.S3MediaStorage'
-
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 django_heroku.settings(locals())
-#heroku!!
+heroku!!
