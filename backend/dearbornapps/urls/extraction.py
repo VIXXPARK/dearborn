52e05c119da6fa30c32b5c62203747a1f6490103
HJinS <windowed@naver.com>
2020-11-08 21:47:42 +0900

    tmp

---
+++ backend/dearbornapps/urls/extraction.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+from dearbornapps.views import extraction
+urlpatterns = [
+    path('feature/getCategory', extraction.getCategory),
+    path('feature/selectFilter', extraction.selectFilter),
+    path('feature/saveType', extraction.saveTasteInfo),
+    
+]
