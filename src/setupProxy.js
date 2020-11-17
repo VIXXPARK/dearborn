32c27e582b79ee3ef19524bd38b281930a68040a
HJinS <windowed@naver.com>
2020-11-17 15:42:56 +0900

    tmp

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,21 +1,21 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function (app) {
-//     app.use(
-//         '/api',
-//         createProxyMiddleware({
-//             target: 'https://dearborn.herokuapp.com',
-//             changeOrigin: true,
-//         })
-//     );
-// };
-
 module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8000',
+            target: 'https://dearborn.herokuapp.com',
             changeOrigin: true,
         })
     );
 };
+
+// module.exports = function (app) {
+//     app.use(
+//         '/api',
+//         createProxyMiddleware({
+//             target: 'http://localhost:8000',
+//             changeOrigin: true,
+//         })
+//     );
+// };
