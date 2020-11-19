531ebb71831ffe52d64be03aa5a55583720a4800
HJinS <windowed@naver.com>
2020-11-19 18:12:23 +0900

    tmp

--- backend/dearbornapps/feature/feature.py
+++ backend/dearbornapps/feature/feature.py
@@ -151,6 +151,7 @@ def GetImageArray(postId):
             dir = dir[-4:]
             path = os.path.join('media',dir[0],dir[1],dir[2],dir[3])
             image = s3Images.from_s3("dearbornstorage",path)
+            image = image.getdata()
             image = tf.image.decode_image(image,channels=3)
             image_array = np.array(image)
             images.append(image_array)
